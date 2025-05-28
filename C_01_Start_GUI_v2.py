63a8bc48588ab14dde8c64131efc1740b338de6c
pringleboo <101750172+pringleboo@users.noreply.github.com>
2025-05-28 13:29:31 +1200

    Mid way through new buttons

---
+++ C_01_Start_GUI_v2.py
@@ -0,0 +1,157 @@
+from tkinter import *
+
+
+class StartQuiz:
+    """
+    Initial Quiz interface (asks users how many rounds they
+    would like to play)
+    """
+
+    def __init__(self):
+        """
+        Gets number of rounds from user
+        """
+
+        # Create frame and setup grid
+        self.start_frame = Frame(padx=10, pady=10)
+        self.start_frame.grid()
+
+        # String for the intro label
+        intro_string = ("In each round you will be given a set of emojis "
+                        "that you must use as clue to guess the movie they represent."
+                        "If you get stuck, click the hint button to get a quote from the movie.")
+
+        background_colour = "#f5ebc1"
+
+        # List of labels to be made (text | font | fg)
+        start_labels_list = [
+            ["Emoji Movie Quiz", ("Arial", "16", "bold"), None],
+            [intro_string, ("Arial", "12"), None],
+            ["How many rounds?", ("Arial", "12", "bold"), "#009900"]
+        ]
+
+        # Create labels and give them names
+        start_label_ref = []
+        for count, item in enumerate(start_labels_list):
+            make_label = Label(self.start_frame, text=item[0], font=item[1],
+                               fg=item[2], bg=background_colour, wraplength=320,
+                               justify="left", pady=10, padx=20)
+            make_label.grid(row=count)
+
+            start_label_ref.append(make_label)
+
+        # Extract changing label so it can be configured later
+        self.changing_label = start_label_ref[2]
+
+        # Frame so that entry box and button can be in the same row
+        self.entry_area_frame = Frame(self.start_frame)
+        self.entry_area_frame.grid(row=3)
+
+        self.num_rounds_entry = Entry(self.entry_area_frame, font=("Arial", "20", "bold"),
+                                      width=20)
+        self.num_rounds_entry.grid(row=0, column=0, padx=10, pady=0)
+
+
+        # Create mode / play buttons
+
+        # # List for buttons (frame | text | bg | command | width | row | column)
+        # play_button_list = [
+        #     [self.normal_button, "Normal Mode", "#1ca1e2", None, 25, 5, 0],
+        #     [self.hard_button, "Hard Mode", "#f0a30d", None, 12, 0, 1],
+        # ]
+        #
+        # # Create buttons and add to list
+        # control_ref_list = []
+        # for item in play_button_list:
+        #     make_control_button = Button(item[0], text=item[1], bg=item[2],
+        #                                  command=item[3], font=("Arial", 16, "bold"),
+        #                                  fg="#FFFFFF", width=item[4])
+        #     make_control_button.grid(row=item[5], column=item[6])
+        #
+        #     control_ref_list.append(make_control_button)
+
+
+        # Create play button...
+        self.play_button = Button(self.entry_area_frame, font=("Arial", "16", "bold"),
+                                  fg="#d99829", bg="#fcfc86", text="Play", width=23,
+                                  command=self.check_rounds, borderwidth=3, relief="raised")
+        self.play_button.grid(row=4, padx=10, pady=10)
+
+
+
+
+        # Change the frame backgrounds
+        self.start_frame.config(bg=background_colour)
+        self.entry_area_frame.config(bg=background_colour)
+
+    # Checks number of rounds enters is a valid input
+    def check_rounds(self):
+
+        # Retrieve temperature to be converted
+        rounds_wanted = self.num_rounds_entry.get()
+
+        # Reset label and entry box (for when users come back to home screen)
+        self.changing_label.config(text="How many rounds?", fg="#009900", font=("Arial", "12", "bold"))
+        self.num_rounds_entry.config(bg="#FFFFFF")
+
+        has_errors = "no"
+
+        # Checks that the amount to be converted is a number above absolute zero
+        try:
+            rounds_wanted = int(rounds_wanted)
+            if rounds_wanted > 0:
+                # Invoke Play Class (and take across number of rounds)
+                Play(rounds_wanted)
+                # Hide root_window (ie: hide rounds choice window)
+                root.withdraw()
+
+            else:
+                has_errors = "yes"
+
+        except ValueError:
+            has_errors = "yes"
+
+        # Display the error if necessary
+        if has_errors == "yes":
+            self.changing_label.config(text="Oops - Please enter a whole number more than 0",
+                                       fg="#990000", font=("Arial", "9", "bold"))
+            self.num_rounds_entry.config(bg="#F4CCCC")
+            self.num_rounds_entry.delete(0, END)
+
+
+class Play:
+    """
+    Interface for playing the Emoji Movie Quiz
+    """
+
+    def __init__(self, how_many):
+        self.play_box = Toplevel()
+
+        self.game_frame = Frame(self.play_box)
+        self.game_frame.grid(padx=10, pady=10)
+
+        self.game_heading_label = Label(self.game_frame, text=f"Round 0 of {how_many}",
+                                        font=("Arial", "16", "bold"))
+        self.game_heading_label.grid(row=0)
+
+        self.end_game_button = Button(self.game_frame, text="End Game",
+                                      font=("Arial", "16", "bold"),
+                                      fg="#FFFFFF", bg="#990000", width="10",
+                                      command=self.close_play)
+        self.end_game_button.grid(row=1)
+
+    # Closes the play GUI
+    def close_play(self):
+        # Reshow root (ie: choose rounds) and end current
+        # quiz / allow new quiz to start
+        root.deiconify()
+        self.play_box.destroy()
+
+
+# Main Routine
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Colour Quest")
+    StartQuiz()
+    root.mainloop()
